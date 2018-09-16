4259f2898da04ce69fcbb0b0c6dc62b9ac33caf8
ReneeHornsby <cynthiahornsby@ymail.com>
2018-09-16 18:18:34 +0000

    Files

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+ 
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
+ 
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+  
+   $(document).ready(function(){
+    $('.tabs').tabs();
+  });
+  
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+  
+    $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
